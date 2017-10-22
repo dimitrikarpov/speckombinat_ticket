becc56ff5a19b0ec41a877a7c36a38e32f7b882c
deeem <karpov.dimon@gmail.com>
2017-10-22 17:13:44 +0300

    add User Ticket models relations

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    /**
+     * Get the user that owns the Ticket
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
