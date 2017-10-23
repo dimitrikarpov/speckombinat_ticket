32a8dc7e3f40e316c96f05dbea08fe91e1a67406
deeem <karpov.dimon@gmail.com>
2017-10-23 12:15:09 +0300

    add Category Ticket relations

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     public $timestamps = false;
+
+    public function tickets()
+    {
+        return $this->hasMany('App\Ticket');
+    }
 }
--- app/Ticket.php
+++ app/Ticket.php
@@ -13,4 +13,12 @@ class Ticket extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    /**
+     * Get the category that ticket belongs to
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
