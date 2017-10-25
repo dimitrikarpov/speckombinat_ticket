699a58c0d91597680b632c0470b86f2b7bebe415
deeem <karpov.dimon@gmail.com>
2017-10-25 10:32:32 +0300

    Ticket list looks like cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,42 +4,33 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
+            <ul class="nav nav-pills nav-justified">
+                @if ($tab == 'todo')
+                    <li role="presentation" class="active"><a href="/home/todo">ToDo</a></li>
+                @else
+                    <li role="presentation"><a href="/home/todo">ToDo</a></li>
+                @endif
 
-                    <ul class="nav nav-pills nav-justified">
-                        @if ($tab == 'todo')
-                            <li role="presentation" class="active"><a href="/home/todo">ToDo</a></li>
-                        @else
-                            <li role="presentation"><a href="/home/todo">ToDo</a></li>
-                        @endif
+                @if ($tab == 'doing')
+                    <li role="presentation" class="active"><a href="/home/doing">Doing</a></li>
+                @else
+                    <li role="presentation"><a href="/home/doing">Doing</a></li>
+                @endif
 
-                        @if ($tab == 'doing')
-                            <li role="presentation" class="active"><a href="/home/doing">Doing</a></li>
-                        @else
-                            <li role="presentation"><a href="/home/doing">Doing</a></li>
-                        @endif
-
-                        @if ($tab == 'done')
-                            <li role="presentation" class="active"><a href="/home/done">Done</a></li>
-                        @else
-                            <li role="presentation"><a href="/home/done">Done</a></li>
-                        @endif
-                    </ul>
-
-                    <hr>
-
-                    @if ($tab == 'todo')
-                        @include ('ticket.list')
-                    @elseif ($tab == 'doing')
-                        @include ('ticket.list')
-                    @elseif ($tab == 'done')
-                        @include ('ticket.list')
-                    @endif
-
-                </div>
-            </div>
+                @if ($tab == 'done')
+                    <li role="presentation" class="active"><a href="/home/done">Done</a></li>
+                @else
+                    <li role="presentation"><a href="/home/done">Done</a></li>
+                @endif
+            </ul>
         </div>
+    </div>
+</div>
+<br>
+<div class="container-fluid">
+    <div class="row">
+
+            @include('ticket.list')
     </div>
 </div>
 @endsection
--- resources/views/ticket/list.blade.php
+++ resources/views/ticket/list.blade.php
@@ -2,36 +2,41 @@
         @if ($tickets->isEmpty())
             <h4>Well, done!</h4>
         @else
-            <ul class="list-group">
             @foreach($tickets as $ticket)
-                <li class="list-group-item">
-                     <p><strong>{{ $ticket-> raised }}</strong> / {{ $ticket->phone}}</p>
-                     <p><a href="#">{{ $ticket->description }}</a></p>
-                     <hr>
-                     @if ($ticket->category_id)
-                        категория: <span class="label label-default">{{ $ticket->category->name }}</span>&nbsp;&nbsp;
-                     @endif
-                     @if ($ticket->priority)
-                        приоритет:
-                        @switch ($ticket->priority)
-                            @case('low')
-                                <span class="label label-info">{{ $ticket->priority }}</span>
-                                @break
-                            @case('normal')
-                                <span class="label label-success">{{ $ticket->priority }}</span>
-                                @break
-                            @case('high')
-                                <span class="label label-danger">{{ $ticket->priority }}</span>
-                                @break
-                        @endswitch
-                     @endif
-                     @if ($ticket->status)
-                        статус: <strong>{{ $ticket->status }}</strong>&nbsp;&nbsp;
-                     @endif
-                     @if ($ticket->user_id)
-                        выполняет: <strong>{{ $ticket->user->name }}</strong>&nbsp;&nbsp;
-                     @endif
-                </li>
+            <div class="col-md-4 col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p><strong>{{ $ticket-> raised }}</strong> / {{ $ticket->phone}}</p>
+                        <p><a href="#">{{ $ticket->description }}</a></p>
+                        <p>
+                         @if ($ticket->priority)
+                            приоритет:
+                            @switch ($ticket->priority)
+                                @case('low')
+                                    <span class="label label-info">{{ $ticket->priority }}</span>
+                                    @break
+                                @case('normal')
+                                    <span class="label label-success">{{ $ticket->priority }}</span>
+                                    @break
+                                @case('high')
+                                    <span class="label label-danger">{{ $ticket->priority }}</span>
+                                    @break
+                            @endswitch
+                         @endif
+                         @if ($ticket->status)
+                            статус: <strong>{{ $ticket->status }}</strong>
+                         @endif
+                        </p>
+                        @if ($ticket->category_id)
+                           <p>категория: <span class="label label-default">{{ $ticket->category->name }}</span></p>
+                        @endif
+                        @if ($ticket->user_id)
+                            <p>выполняет: <strong>{{ $ticket->user->name }}</strong></p>
+                        @endif
+                    </div><!-- panel-body -->
+                </div><!-- panel -->
+
+
+             </div><!-- col -->
             @endforeach
-            </ul>
         @endif
