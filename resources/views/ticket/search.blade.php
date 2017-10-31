0a4ec8231e5c043c0c3cb00db9e754f9c86ea428
deeem <karpov.dimon@gmail.com>
2017-10-31 19:02:50 +0200

    search

--- resources/views/ticket/search.blade.php
+++ resources/views/ticket/search.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
 @php
@@ -7,10 +7,10 @@
     $category_id = app('request')->input('category_id');
     $user_id = app('request')->input('user_id');
 @endphp
-<div class="container">
 
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <h1 class="page-header">Фильтр по выполненным</h1>
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">
                         from: {{ $date_from }}
@@ -74,7 +74,7 @@
     </div><!-- row -->
 
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <table class="table">
                     <tr>
@@ -108,5 +108,4 @@
         </div>
     </div>
 
-</div>
 @endsection
