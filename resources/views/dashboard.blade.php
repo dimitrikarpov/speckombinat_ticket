1346203924714b361526297c11a39b35e173f0ab
deeem <karpov.dimon@gmail.com>
2017-10-31 09:58:38 +0200

    tickets loop

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,11 +11,13 @@
 <br>
 <div class="container-fluid">
     <div class="row">
-        @if ($tickets->isEmpty())
+
+        @forelse ($tickets as $ticket)
+            @include('ticket.single')
+        @empty
             <h4>Well, done!</h4>
-        @else
-            @include('ticket.list')
-        @endif
+        @endforelse
+        
     </div>
 </div>
 @endsection
