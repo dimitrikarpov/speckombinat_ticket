0d18a090f0e031662431a4835f02ae593647d47e
deeem <karpov.dimon@gmail.com>
2017-11-23 19:18:12 +0200

    fix unauthenticated user add ticket form layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,10 @@
             </div>
         </nav>
 
-        @yield('content')
+        <div class="container">
+          @yield('content')
+        </div>
+
     </div>
 
     <!-- Scripts -->
